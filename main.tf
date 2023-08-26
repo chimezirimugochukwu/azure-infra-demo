a8d035ec392eb0997fe0fb7380da2b68d4483ff8
chimezirimugochukwu <113891267+chimezirimugochukwu@users.noreply.github.com>
2023-08-26 13:45:25 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true  # Required for Linux
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "myuniqueprefix-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+site_config {
+    linux_fx_version = "DOCKER|nginx:latest"
+}
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+    "WEBSITES_PORT" = "8080" 
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+
+resource "azurerm_role_assignment" "example" {
+  principal_id         = azurerm_app_service.example.identity.0.principal_id
+  role_definition_name = "Contributor"
+  scope                = azurerm_resource_group.example.id
+}
